d6d43eb98dc15643d905377c8e5a3e2ca52b9a48
Justin <Justin@Vap0r-Helix>
2016-10-16 13:18:18 -0500

    another typo fix

--- skillz/CS200/src/homework5/Problem5_1.java
+++ skillz/CS200/src/homework5/Problem5_1.java
@@ -13,8 +13,12 @@ public class Problem5_1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/**
+		 *  is input 0? if not keep getting input
+		 *  count amount of inputted positive #s & negative #s (exclude 0)
+		 *  compute sum and average of all #'s (exclude 0)
+		 */
+		
 	}
 
 }
