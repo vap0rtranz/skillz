1788c002720be5d23f52e4d0a0ffccc4c38ab86b
Jp <vap0rtranz@gmail.com>
2017-10-11 23:09:58 -0500

    cleanup of SP as OUTPUT
    adding OUTPUT parameters to SP

--- CMIT200/exec-sp-insert-customer.sql
+++ CMIT200/exec-sp-insert-customer.sql
@@ -1,5 +1,10 @@
 USE CraftStoreDB_DEV;
 
+-- setup variable to capture OUTPUT
+DECLARE @CustomerIDOut INT;
+
 EXEC dbo.InsertCustomer 
 @FirstName = 'First', 
-@LastName = 'Last';
+@LastName = 'Last',
+-- capture output parameter labelled CustomerID
+@CustomerID = @CustomerIDOut OUTPUT;
